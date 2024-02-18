8f5b0585ae8ec616b4e31f20f2b3d9818a432e62
Oliver Caldwell <ollie@oli.me.uk>
2024-02-18 12:57:43 +0000

    Add testing tools

--- .nvim.lua
+++ .nvim.lua
@@ -1,4 +1,16 @@
 -- [nfnl] Compiled from .nvim.fnl by https://github.com/Olical/nfnl, do not edit.
-package.path = (package.path .. ";test/lua/?.lua")
+local _local_1_ = require("nfnl.module")
+local autoload = _local_1_["autoload"]
+local reload = autoload("plenary.reload")
+local notify = autoload("nfnl.notify")
+vim.api.nvim_set_keymap("n", "<localleader>pt", "<Plug>PlenaryTestFile", {desc = "Run the current test file with plenary."})
+vim.api.nvim_set_keymap("n", "<localleader>pT", "<cmd>PlenaryBustedDirectory lua/conjure-spec/<cr>", {desc = "Run all tests with plenary."})
+local function _2_()
+  notify.info("Reloading...")
+  reload.reload_module("conjure")
+  require("conjure.main")
+  return notify.info("Done!")
+end
+vim.api.nvim_set_keymap("n", "<localleader>pr", "", {desc = "Reload the conjure modules.", callback = _2_})
 --[[ (nvim.ex.augroup "conjure_set_state_key_on_dir_changed") (nvim.ex.autocmd_) (nvim.ex.autocmd "DirChanged * call luaeval(\"require('conjure.client')['set-state-key!']('\" . getcwd() . \"')\")") (nvim.ex.augroup "END") ]]
 return nil
